8cac604d82b9cca03900c0fd43638ecdcfeb3375
lxbarth <alex@mapbox.com>
2014-01-29 15:56:09 -0500

    update to latest dom changes.

--- mapbox.js
+++ mapbox.js
@@ -2,13 +2,13 @@ function edit() {
     d3.event.preventDefault();
     d3.event.stopPropagation();
     var params = window.location.hash.substring(1).split('/');
-    window.location.href = 'http://openstreetmap.us/iD/master/#map=' +
-        params[0] + '/' + params[2] + '/' + params[1];
+    window.location.href = 'http://www.openstreetmap.org/edit#map=' +
+        params[0] + '/' + params[1] + '/' + params[2];
 }
 
-d3.select('.leaflet-control-attribution')
+d3.select('.mapbox-control-info')
     .text(function() { return d3.select(this).text() + ' '; })
     .append('a')
-    .attr('href', 'http://openstreetmap.us/iD/master/')
-    .text('Edit on OSM')
+    .attr('href', 'http://www.openstreetmap.org/edit')
+    .text('Improve this map')
     .on('click', edit);
